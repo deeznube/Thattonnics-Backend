bc8c3bd6dd597a4a0dfb73b64c2673d51e313def
ercusz <eurokungz.xkz@gmail.com>
2021-10-30 19:00:17 +0700

    userinfo request mapping, ignore user password
    time to sleep😪

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,15 +1,29 @@
 package com.example.demo.controllers;
 
+import java.util.Optional;
+
+import com.example.demo.ResourceNotFound;
+import com.example.demo.models.User;
+import com.example.demo.repository.UserInfoRepository;
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+	@Autowired
+	UserRepository userRepository;
+
     @GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
@@ -33,5 +47,12 @@ public class UserController {
 		return "Admin Board.";
 	}
 
-	
+	@GetMapping("/user-info")
+	@PreAuthorize("hasRole('USER') or hasRole('EMPLOYEE') or hasRole('ADMIN')")
+	public ResponseEntity<User> getUserInfo(@RequestParam(value="username")String username) throws ResourceNotFound {
+		Optional<User> user = userRepository.findByUsername(username);
+
+		return ResponseEntity.ok().body(user.get());
+	}
+
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -20,6 +20,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -45,6 +46,7 @@ public class User {
 
 	@NotBlank
 	@Size(max = 120)
+	@JsonIgnore
 	private String password;
 
 	@ManyToMany(fetch = FetchType.LAZY)
